d2623bc79ede862dbd82e510d3aede2ba98a1294
jrai51 <87856634+jrai51@users.noreply.github.com>
2023-02-08 21:12:30 -0500

    nary done

--- test.py
+++ test.py
@@ -95,4 +95,61 @@ def binary_ne_grid(cagey_grid):
     return (binary_csp, vars)
 
 
-print(binary_ne_grid([3, [(3,[(1,1), (2,1)],"+"),(1, [(1,2)], '?'), (8, [(1,3), (2,3), (2,2)], "+"), (3, [(3,1)], '?'), (3, [(3,2), (3,3)], "+")]]))
+def X_binary_ne_grid(cagey_grid):
+    binary_csp = CSP("BinaryCSP") #construct csp 
+    vars = []
+    N = cagey_grid[0]
+
+    #Create all variables (cells in the grid)
+    for i in range (1, N+1):
+        for j in range(1, N+1):
+            dom = range(1, N+1)
+            var_name = f"Cell({i},{j})"
+            var = Variable(var_name, dom)
+            vars.append(var)
+
+   #cages = cagey_csp_model[1]
+
+    valid_bin_tuples = [(i, j) for i in range(1, N+1) 
+                        for j in range(1, N+1) 
+                        if i != j ]
+    
+    #No two cells in a row can be the same
+    #rows = [vars[x:x+N] for x in range(0, len(vars), N)]
+
+
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+
+            # Add column constraints
+            for j2 in range(j+1, N+1):
+                con = Constraint(
+                    f"NEQ(({i},{j}),({i},{j2}))",
+                    [
+                        vars[_cor_to_idx(i,j,N)],
+                        vars[_cor_to_idx(i,j2,N)]
+                    ]
+                )
+                con.add_satisfying_tuples(valid_bin_tuples)
+                binary_csp.add_constraint(con)
+            
+            # Add row constraints
+            for i2 in range(i+1, N+1):
+                con = Constraint(
+                    f"NEQ(({i},{j}),({i2},{j}))",
+                    [
+                        vars[_cor_to_idx(i,j,N)],
+                        vars[_cor_to_idx(i2,j,N)]
+                    ]
+                )
+                con.add_satisfying_tuples(valid_bin_tuples)
+                binary_csp.add_constraint(con)
+
+
+    return (binary_csp, vars)
+
+csp, vars = binary_ne_grid([3, [(3,[(1,1), (2,1)],"+"),(1, [(1,2)], '?'), (8, [(1,3), (2,3), (2,2)], "+"), (3, [(3,1)], '?'), (3, [(3,2), (3,3)], "+")]])
+
+print()
+print(vars)
+print(csp.get_all_cons())
